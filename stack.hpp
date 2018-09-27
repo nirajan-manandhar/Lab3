c7f6d216e489ccbdfba523bab683d9e6d2985e09
nirajan-manandhar <manandnirajan@gmail.com>
2018-09-27 10:50:08 -0700

    Created constructor for stack class and member functions push, pop and top.

--- stack.hpp
+++ stack.hpp
@@ -10,10 +10,30 @@ class stack
 public:
     static constexpr int array_max = 9;
 
+    stack(){
+        topIndex = -1;
+    }
 
+    bool push(int newValue){
+        if(topIndex == 9) {
+            return false;
+        } else {
+            topIndex = topIndex + 1;
+            Array[topIndex] = newValue;
+        }
+        return true;
+    }
+
+    void pop(){
+        topIndex -= topIndex;
+    }
+
+    const int top(){
+
+    }
 
 private:
-    int Array[array_max]; //Int array of size 10
+    int Array[array_max]{}; //Int array of size 10
     int topIndex; //Stores the index of the top of the stack
 };
 
